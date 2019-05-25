c715e91424589c4376786c7b0e1a5cc8d28ef17d
fingoldin <vassilioskaxiras@gmail.com>
2019-05-25 19:56:45 +0300

    update version number

--- corels/__init__.py
+++ corels/__init__.py
@@ -1,6 +1,6 @@
 from .corels import CorelsClassifier
 from .utils import load_from_csv, RuleList
 
-__version__ = "1.1.8"
+__version__ = "1.1.11"
 
 __all__ = ["CorelsClassifier", "load_from_csv", "RuleList"]
--- tests/test.py
+++ tests/test.py
@@ -1,21 +1,7 @@
 from corels import *
-from interpret import show
-from interpret.blackbox import LimeTabular
-from interpret.perf import ROC
-import time
 
 X, y, features = load_from_csv("data/compas.csv")
 c = CorelsClassifier(n_iter=100000)
 
 a = c.fit(X, y, features=features).score(X, y)
 c.printrl()
-
-#roc = ROC(c.predict).explain_perf(X, y, name="ROC")
-#show(roc)
-
-lime = LimeTabular(predict_fn=c.predict, feature_names=features, data=X)
-lime_explain = lime.explain_local(X[:1], y[:1])
-
-show(lime_explain)
-
-time.sleep(100)
