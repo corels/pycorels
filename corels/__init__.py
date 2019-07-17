66c2e10afb6c0c647d2fac47fddbb0df995036c7
fingoldin <vassilioskaxiras@gmail.com>
2019-07-17 03:27:00 +0300

    update manifest

--- corels/__init__.py
+++ corels/__init__.py
@@ -2,6 +2,6 @@ from __future__ import print_function, division, with_statement
 from .corels import CorelsClassifier
 from .utils import load_from_csv, RuleList
 
-__version__ = "1.1.20"
+__version__ = "1.1.21"
 
 __all__ = ["CorelsClassifier", "load_from_csv", "RuleList"]
